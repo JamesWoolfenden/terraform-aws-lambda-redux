8ecbd059ce8b10f85c6ab3694fdc206e23781f2b
James Woolfenden <James.Woolfenden@gmail.com>
2021-03-20 16:53:00 +0000

    refresh

--- data.aws_iam_policy_document.lambda.tf
+++ data.aws_iam_policy_document.lambda.tf
@@ -1,4 +1,9 @@
 data "aws_iam_policy_document" "lambda" {
+  #checkov:skip=CKV_AWS_108
+  #checkov:skip=CKV_AWS_109
+  #checkov:skip=CKV_AWS_110
+  #checkov:skip=CKV_AWS_111
+
 
   statement {
     actions = [
